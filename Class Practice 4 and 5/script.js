7baf0ccc8d3fe22cd7839d89de5476efafcae754
Muhammad-Mahad111 <mahad.pk111@gmail.com>
2026-06-18 17:01:38 +0500

    Updated Class 4 and 5 Practice

---
+++ Class Practice 4 and 5/script.js
@@ -0,0 +1,75 @@
+// Question 1 
+
+let a = 5;
+let b = 3;
+
+console.log("Question 1: \n a: " + a + "\n b: " + b + "\nQuestion: a + b-- * ++a + b / --b - a++ \nwhat will be the value of result, a and b")
+
+let result_1 = a + b-- * ++a + b / --b - a++;
+
+console.log("result: ", result_1);
+console.log("a: ", a);
+console.log("b: ", b);
+
+// Question 2 
+
+let x = 4;
+let y = 2;
+console.log("Question 2: \n x: " + x + "\n y: " + y + "\nQuestion: x++ + --y * ++x - y++ + x / y \nwhat will be the value of result, x and y")
+
+let result_2 = x++ + --y * ++x - y++ + x / y;
+
+console.log("result: ", result_2);
+console.log("x: ", x);
+console.log("y: ", y);
+
+// Question 3
+
+let c = 6;
+let d = 4;
+console.log("Question 3: \n c: " + c + "\n d: " + d + "\nQuestion: --c + d++ * c - ++d + c-- / d \nwhat will be the value of result, c and d")
+
+let result_3 = --c + d++ * c - ++d + c-- / d;
+
+console.log("result: ", result_3);
+console.log("c: ", c);
+console.log("d: ", d);
+
+
+// Question 4
+
+let m = 8;
+let n = 5;
+console.log("Question 4: \n m: " + m + "\n n: " + n + "\nQuestion: ++m - n-- + m * --n + n++ - m-- / d \nwhat will be the value of result, m and n")
+
+let result_4 = ++m - n-- + m * --n + n++ - m--;
+
+console.log("result: ", result_4);
+console.log("m: ", m);
+console.log("n: ", n);
+
+
+// Question 5
+
+let p = 7;
+let q = 3;
+console.log("Question 5: \n p: " + p + "\n q: " + q + "\nQuestion: p++ * ++q - --p + q-- + p / q \nwhat will be the value of result, p and q")
+
+let result_5 = p++ * ++q - --p + q-- + p / q;
+
+console.log("result: ", result_5);
+console.log("p: ", p);
+console.log("q: ", q);
+
+
+// Question 6
+
+let x6 = 10;
+let y6 = 4;
+console.log("Question 6: \n x6: " + x6 + "\n y6: " + y6 + "\nQuestion: ++x6 + y6-- * x6++ - --y6 + x6 / ++y6 - x6-- \nwhat will be the value of result, x6 and q")
+
+let result_6 = ++x6 + y6-- * x6++ - --y6 + x6 / ++y6 - x6--;
+
+console.log("result: ", result_6);
+console.log("x6: ", x6);
+console.log("y6: ", y6);
